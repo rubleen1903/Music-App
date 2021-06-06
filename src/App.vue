a7a6ef307d1a5260da09bd7bd8b304d528be1cb8
rubleen1903 <rubleen16@gmail.com>
2021-06-06 17:48:09 +0530

    final commit

---
+++ src/App.vue
@@ -0,0 +1,218 @@
+<template>
+  <div id="app">
+    <header class="nav">
+      <h1>My Music App by Rubleen Kaur</h1>
+    </header>
+    <main>
+      <section class="player">
+        <h2 class="song-title">{{ current.title }} - <span>{{ current.artist }}</span></h2>
+        <div class="controls">
+          <button class="prev" @click="prev">Prev</button>
+          <button class="play" v-if="!isPlaying" @click="play">Play</button>
+          <button class="pause" v-else @click="pause">Pause</button>
+          <button class="next" @click="next">Next</button>
+        </div>
+      </section>
+      <section class="playlist">
+        <h3>The Playlist</h3>
+        <button v-for="song in songs" :key="song.src" @click="play(song)" :class="(song.src == current.src) ? 'song playing' : 'song'">
+          {{ song.title }} - {{ song.artist }}
+        </button>
+      </section>
+    </main>
+    
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      current: {},
+      index: 0,
+      isPlaying: false,
+      
+      songs: [
+        {
+          title: 'I love you baby',
+          artist: 'Emilee',
+          src: require('./assets/ily.mp3')
+        },
+        {
+          title: 'Its You',
+          artist: 'Ali Gatie',
+          src: require('./assets/itsu.mp3')
+        },
+        {
+          title: 'Arcade',
+          artist: 'Duncan Laurence',
+          src: require('./assets/arcade.mp3')
+        },
+         {
+          title: 'Old Town Road',
+          artist: 'Lil Nax',
+          src: require('./assets/oldtown.mp3')
+        },
+         {
+          title: 'Drivers License',
+          artist: 'Olivia Rodrigo',
+          src: require('./assets/driverlicense.mp3')
+        },
+         {
+          title: 'Night Changes',
+          artist: 'One Direction',
+          src: require('./assets/nightchanges.mp3')
+        }
+      ],
+      player: new Audio()
+    }
+  },
+  methods: {
+    play (song) {
+      if (typeof song.src != "undefined") {
+        this.current = song;
+        this.player.src = this.current.src;
+      }
+      this.player.play();
+      this.player.addEventListener('ended', function () {
+        this.index++;
+        if (this.index > this.songs.length - 1) {
+          this.index = 0;
+        }
+        this.current = this.songs[this.index];
+        this.play(this.current);
+      }.bind(this));
+      this.isPlaying = true;
+    },
+    pause () {
+      this.player.pause();
+      this.isPlaying = false;
+    },
+    next () {
+      this.index++;
+      if (this.index > this.songs.length - 1) {
+        this.index = 0;
+      }
+      this.current = this.songs[this.index];
+      this.play(this.current);
+    },
+    prev () {
+      this.index--;
+      if (this.index < 0) {
+        this.index = this.songs.length - 1;
+      }
+      this.current = this.songs[this.index];
+      this.play(this.current);
+    }
+  },
+  created () {
+    this.current = this.songs[this.index];
+    this.player.src = this.current.src;
+  }
+}
+</script>
+
+<style>
+
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+  /* background-color: #dffbfa; */
+}
+body{
+  font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+}
+header {
+	display: flex;
+	padding: 15px;
+	background-color: #4d486c;
+	color: #b0c5d9;
+}
+
+h1{
+  background-color: #4d486c;
+  text-align: center;
+  margin:0;
+  justify-content: center;
+}
+main {
+  width: 100%;
+  max-width: 768px;
+  margin: 0 auto;
+  padding: 25px;
+}
+.song-title {
+  color: #53565A;
+  font-size: 32px;
+  font-weight: 700;
+  text-transform: uppercase;
+  text-align: center;
+}
+.song-title span {
+  font-weight: 400;
+  font-style: italic;
+}
+.controls {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 30px 15px;
+}
+button {
+  appearance: none;
+  background: none;
+  border: none;
+  outline: none;
+  cursor: pointer;
+}
+button:hover {
+  opacity: 0.8;
+}
+.play, .pause {
+  font-size: 20px;
+  font-weight: 700;
+  padding: 15px 25px;
+  margin: 0px 15px;
+  border-radius: 8px;
+  color: #FFF;
+  background-color: #8975a2;
+}
+.next, .prev {
+  font-size: 16px;
+  font-weight: 700;
+  padding: 10px 20px;
+  margin: 0px 15px;
+  border-radius: 6px;
+  color: #FFF;
+  background-color: #8975a2;
+}
+.playlist {
+  padding: 0px 30px;
+}
+.playlist h3 {
+  color: #212121;
+  font-size: 28px;
+  font-weight: 400;
+  margin-bottom: 30px;
+  text-align: center;
+}
+.playlist .song {
+  display: block;
+  width: 100%;
+  padding: 15px;
+  font-size: 20px;
+  font-weight: 700;
+  cursor: pointer;
+}
+.playlist .song:hover {
+  color: #4d486c;
+}
+.playlist .song.playing {
+  color: #FFF;
+  background-image: linear-gradient(to right, #4d486c, #b0c5d9);
+}
+
+</style>
